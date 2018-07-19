f4b380f0af82e82ec5db8422cf57f6052fcefd50
Anna Royer <anna.royer@colorado.edu>
2018-07-19 17:01:45 -0600

    add test it finds horizontal distance

--- test/grid_test.rb
+++ test/grid_test.rb
@@ -6,6 +6,12 @@ class GridTest < Minitest::Test
   def test_it_finds_vertical_distance
     grid = Grid.new(3, ["p--", "-m-", "---"])
 
-    assert_equal -1, grid.vertical_distance
+    assert_equal (-1), grid.vertical_distance
+  end
+
+  def test_it_finds_horizontal_distance
+    grid = Grid.new(3, ["p--", "-m-", "---"])
+
+    assert_equal (-1), grid.horizontal_distance
   end
 end

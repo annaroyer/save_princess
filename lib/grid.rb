65a23acf2a819686de77fa3c23501bda034f5eb0
Anna Royer <anna.royer@colorado.edu>
2018-07-20 11:04:39 -0600

    adds next_move method and vector_path method, refactors vertical and horizontal mothods to return nil if princess and bot are in the same column or row

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,23 +1,37 @@
 class Grid
-  def initialize(size, rows)
+  def initialize(size, bot_row=size/2, bot_column=size/2, rows)
     @size = size
-    @distance = size / 2
-    @princess_corner = rows.join.index('p')
+    @bot_coordinates = [bot_row, bot_column]
+    @distance = @size / 2
+    @princess_space = rows.join.index('p')
+  end
+
+  def princess_coordinates
+    [@princess_space / @size, @princess_space % @size]
+  end
+
+  def vector_path
+    princess_coordinates.zip(@bot_coordinates).map do |coordinates|
+      coordinates.reduce(:-)
+    end
   end
 
   def vertical
-    return 'UP' if (@princess_corner / @size).zero?
-    'DOWN'
+    return 'DOWN' if vector_path.first.positive?
+    return 'UP' if vector_path.first.negative?
   end
 
   def horizontal
-    return 'LEFT' if (@princess_corner % @size).zero?
-    'RIGHT'
+    return 'RIGHT' if vector_path.last.positive?
+    return 'LEFT' if vector_path.last.negative?
   end
 
   def full_path
-    Array.new(@distance, vertical)
-    .concat(Array.new(@distance, horizontal))
+    Array.new(@distance, vertical).concat(Array.new(@distance, horizontal))
     .join("\n")
+  end
+
+  def next_move
+    vertical || horizontal
   end
 end

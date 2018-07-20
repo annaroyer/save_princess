2547ea08d4cfb377fe68de374e932c12b8e9c5a6
Anna Royer <anna.royer@colorado.edu>
2018-07-19 18:09:12 -0600

    rename slots to spaces

--- lib/grid.rb
+++ lib/grid.rb
@@ -2,18 +2,18 @@ class Grid
   def initialize(size, rows)
     @size = size
     @characters = ['p', 'm']
-    @slots = rows.join.chars
+    @spaces = rows.join
   end
 
   def vertical_distance
     @characters.map do |character|
-      @slots.find_index(character) / @size
+      @spaces.index(character) / @size
     end.reduce(:-)
   end
 
   def horizontal_distance
     @characters.map do |character|
-      @slots.find_index(character) % @size
+      @spaces.index(character) % @size
     end.reduce(:-)
   end
 

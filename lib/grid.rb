8be41a57b1789676da0d5a8d0cd0d8f2787f14dc
Anna Royer <anna.royer@colorado.edu>
2018-07-20 11:25:27 -0600

    fix coordinates to be in x, y order and change distance to the actual horizontal and vertical distance to get test passing

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,13 +1,13 @@
 class Grid
   def initialize(size, bot_row=size/2, bot_column=size/2, rows)
     @size = size
-    @bot_coordinates = [bot_row, bot_column]
+    @bot_coordinates = [bot_column, bot_row]
     @distance = @size / 2
     @princess_space = rows.join.index('p')
   end
 
   def princess_coordinates
-    [@princess_space / @size, @princess_space % @size]
+    [@princess_space % @size, @princess_space / @size]
   end
 
   def vector_path
@@ -17,18 +17,18 @@ class Grid
   end
 
   def vertical
-    return 'DOWN' if vector_path.first.positive?
-    return 'UP' if vector_path.first.negative?
+    return 'DOWN' if vector_path.last.positive?
+    return 'UP' if vector_path.last.negative?
   end
 
   def horizontal
-    return 'RIGHT' if vector_path.last.positive?
-    return 'LEFT' if vector_path.last.negative?
+    return 'RIGHT' if vector_path.first.positive?
+    return 'LEFT' if vector_path.first.negative?
   end
 
   def full_path
-    Array.new(@distance, vertical).concat(Array.new(@distance, horizontal))
-    .join("\n")
+    Array.new(vector_path.last.abs, vertical).concat(Array.new(vector_path.first.abs, horizontal))
+    .compact.join("\n")
   end
 
   def next_move

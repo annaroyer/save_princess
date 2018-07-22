d11f4ca91b24d2ba02a620b56c1499c6c16aaaca
Anna Royer <anna.royer@colorado.edu>
2018-07-22 11:40:00 -0600

    find princess row and column in initialize, and remove unnecessary vector_path, simplify vertical and horizontal methods and change full_path method to utilize these

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,37 +1,26 @@
 class Grid
-  def initialize(size, bot_row=size/2, bot_column=size/2, rows)
-    @size = size
-    @bot_coordinates = [bot_column, bot_row]
-    @distance = @size / 2
-    @princess_space = rows.join.index('p')
-  end
-
-  def princess_coordinates
-    [@princess_space % @size, @princess_space / @size]
-  end
-
-  def vector_path
-    princess_coordinates.zip(@bot_coordinates).map do |coordinates|
-      coordinates.reduce(:-)
-    end
-  end
-
-  def vertical
-    return 'DOWN' if vector_path.last.positive?
-    return 'UP' if vector_path.last.negative?
-  end
-
-  def horizontal
-    return 'RIGHT' if vector_path.first.positive?
-    return 'LEFT' if vector_path.first.negative?
+  def initialize(size, rows)
+    @from_center = size / 2
+    @princess_space = rows.map(&:chomp).join.index('p')
+    @princess_row = @princess_space / size
+    @princess_column = @princess_space % size
   end
 
   def full_path
-    Array.new(vector_path.last.abs, vertical).concat(Array.new(vector_path.first.abs, horizontal))
-    .compact.join("\n")
+    Array.new(@from_center, vertical) + Array.new(@from_center, horizontal)
   end
 
-  def next_move
-    vertical || horizontal
+  def next_move(bot_row, bot_column)
+    vertical(bot_row) || horizontal(bot_column)
+  end
+
+  def vertical(bot_row = @from_center)
+    return 'UP' if @princess_row < bot_row
+    return 'DOWN' if @princess_row > bot_row
+  end
+
+  def horizontal(bot_column = @from_center)
+    return 'LEFT' if @princess_column < bot_column
+    return 'RIGHT' if @princess_column > bot_column
   end
 end

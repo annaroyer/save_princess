6807744259a04c7e5120fb4324451537f77e46cd
Anna Royer <anna.royer@colorado.edu>
2018-07-22 11:37:45 -0600

    add new lines to ends of rows to match hacker rank input, remove full path for randomly placed test b/c not part of requirements at the moment and add arguments to next_move call in test

--- test/grid_test.rb
+++ test/grid_test.rb
@@ -4,32 +4,26 @@ require './lib/grid'
 
 class GridTest < Minitest::Test
   def test_it_finds_the_vertical_direction_to_the_princess
-    grid = Grid.new(3, ["p--", "-m-", "---"])
+    grid = Grid.new(3, ["p--\n", "-m-\n", "---"])
 
     assert_equal 'UP', grid.vertical
   end
 
   def test_it_finds_the_horizontal_direction_to_the_princess
-    grid = Grid.new(3, ["p--", "-m-", "---"])
+    grid = Grid.new(3, ["p--\n", "-m-\n", "---"])
 
     assert_equal 'LEFT', grid.horizontal
   end
 
   def test_it_prints_the_full_path_to_the_princess
-    grid = Grid.new(3, ["p--", "-m-", "---"])
+    grid = Grid.new(3, ["p--\n", "-m-\n", "---"])
 
-    assert_equal "UP\nLEFT", grid.full_path
+    assert_equal ["UP", "LEFT"], grid.full_path
   end
 
   def test_it_prints_the_next_move_to_the_princess
-    grid = Grid.new(5, 2, 3, ['-----','-----', 'p--m-', '-----', '-----'])
+    grid = Grid.new(5, ["-----\n","-----\n", 'p--m-', "-----\n", "-----"])
 
-    assert_equal "LEFT", grid.next_move
-  end
-
-  def test_it_finds_the_princess_when_bot_and_princess_are_randomly_placed
-    grid = Grid.new(5, 2, 3, ['-----','-----', 'p--m-', '-----', '-----'])
-
-    assert_equal "LEFT\nLEFT\nLEFT", grid.full_path
+    assert_equal "LEFT", grid.next_move(2, 3)
   end
 end

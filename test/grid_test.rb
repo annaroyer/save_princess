4f1a4caba29b0d57b2de733df8fcf5161db52a5b
Anna Royer <anna.royer@colorado.edu>
2018-07-25 09:44:41 -0600

    remove tests for private methods

--- test/grid_test.rb
+++ test/grid_test.rb
@@ -3,26 +3,14 @@ require 'minitest/pride'
 require './lib/grid'
 
 class GridTest < Minitest::Test
-  def test_it_finds_the_vertical_direction_to_the_princess
-    grid = Grid.new(3, ["p--\n", "-m-\n", "---"])
-
-    assert_equal 'UP', grid.vertical
-  end
-
-  def test_it_finds_the_horizontal_direction_to_the_princess
-    grid = Grid.new(3, ["p--\n", "-m-\n", "---"])
-
-    assert_equal 'LEFT', grid.horizontal
-  end
-
   def test_it_prints_the_full_path_to_the_princess
-    grid = Grid.new(3, ["p--\n", "-m-\n", "---"])
+    grid = Grid.new(3, ["p--", "-m-", "---"])
 
     assert_equal ["UP", "LEFT"], grid.full_path
   end
 
   def test_it_prints_the_next_move_to_the_princess
-    grid = Grid.new(5, ["-----\n","-----\n", 'p--m-', "-----\n", "-----"])
+    grid = Grid.new(5, ["-----","-----", 'p--m-', "-----", "-----"])
 
     assert_equal "LEFT", grid.next_move(2, 3)
   end
